ba4ca5007a76ff7ea010d654ea72815badc97c80
MaxxRose <90778837+MaxxRose@users.noreply.github.com>
2022-04-08 11:22:51 -0400

    sd

---
+++ python.py
@@ -0,0 +1,30 @@
+from itertools import pairwise
+
+
+#user_dictionary=["max"]
+#user_name= "max"
+#if user_name in user_dictionary:
+#    print("userfound")
+#else:
+#    print("user not found")
+
+#numbers = range(1,1000001)
+#for number in numbers: 
+#    print(number)
+
+#number = list (range(1,000))
+#print (numbers.max())
+
+ #dictionary: sequence of key_value pairs
+ users= {"user1": "zane",
+ "user2" : "jinu",
+ "user3" :"Umika",
+    }
+print(users["user2"])
+users["user1"] = "lane"
+users ["user4"] = "Max"
+print(users)
+
+for key, in users.items
+
+
