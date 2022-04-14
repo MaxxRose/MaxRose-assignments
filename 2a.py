c8b48545b78be9512e63d056ca2eb3353ce224c9
MaxxRose <90778837+MaxxRose@users.noreply.github.com>
2022-04-14 11:46:03 -0400

    2a2b2c

---
+++ 2a.py
@@ -0,0 +1,7 @@
+text_messages= ["ok","cool","what"]
+    def show_messages (text_message):
+        if "ok","cool","what" in text_messages:
+            print(text_message)
+for text in text_messages:
+    print(text)
+show_messages(text_messages)
---
+++ 2b.py
@@ -0,0 +1,6 @@
+sent_messages=[]
+def send messages():
+    for text_message in text_message:
+        sent_messages.append(text_message)
+        print(text_message, sent_messages)
+    send_messages()
